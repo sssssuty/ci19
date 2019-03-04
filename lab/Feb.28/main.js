99e9eab0d23393b04680e2542a879fa585509161
Tingyu Su <tingyusu@Tingyus-MBP.fios-router.home>
2019-03-04 16:31:50 -0500

    labhomework2

---
+++ lab/Feb.28/main.js
@@ -0,0 +1,22 @@
+// while (i < 5) {
+//     // $(document).ready(function() {
+//     //     body.append('<div></div>');
+//     // });
+
+
+    
+    
+//     i++;
+// }
+
+
+
+
+var thing = document.createElement("div");
+
+    thing.style.backgroundColor = "red";
+
+    document.body.appendChild("thing");
+
+
+    thing.setAttribute("shirt", "black");

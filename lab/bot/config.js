6b5b71cb60876b32cf65ad0ebf932f26f00672d4
Tingyu Su <tingyusu@Tingyus-MBP.fios-router.home>
2019-05-09 22:06:24 -0400

    onemoretry

--- lab/bot/config.js
+++ lab/bot/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key:"",
-    consumer_secret:"",
-    access_token:"",
-    access_token_secret:""
+    consumer_key:"nBWZpRMEBObZj8KndvYNjdpav",
+    consumer_secret:"2lYfs3UqlFD084qfi2nKu01xuuWkcW78PQLGaoGpktcJLq895N",
+    access_token:"1126272635652931584-ye23l5ydUJpJqlqt5Wb23IRBGIv0kt",
+    access_token_secret:"ieVc4qC1l9LixYGbA24aYEBFlAWDrCEiMHDNLyLyqdcF0"
 }
--- lab/bot/index.js
+++ lab/bot/index.js
@@ -1,4 +1,4 @@
-var twitter_handle = "";
+const twitter_handle = "sut314";
 
 var Twit = require("twit");
 var config = require("./config");
@@ -6,3 +6,40 @@ var config = require("./config");
 var T = new Twit(config);
 
 console.log("The bot is starting...");
+
+function sendTweet(message) {
+    var tweet = {status: message};
+    T.post("statuses/update", tweet, tweeted);
+
+    function tweeted(error, data, reponse){
+        if(error){
+            console.log(error);
+        }
+        // console.log(data);
+        console.log("tweeted", message);
+    }
+}
+
+//sending one tweet when th app boots up
+// sendTweet("🐯");
+
+//once every 24hours
+// setInterval(function(){}, 1000 * 60 * 60 * 24)
+function randInt(max){
+    return Math.floor( Math.random() * max );
+}
+
+function gardenTweet(){
+    var chars = ["🐙","🦀","🐡","🐳","🐟","🦑","🦐","🦈","🐊","🐠","🐋","🐯","🐯","🐯","🐯"," "," "," "," "," "," "," "," "," "," ",""];
+    var garden = "";
+
+    for (var i = 0; i < 25; i += 1){
+        garden = garden + chars[randInt(chars.length)];
+    }
+    sendTweet(garden);
+}
+
+setInterval(gardenTweet, 1000 * 60 * 10);
+gardenTweet();
+
+

937129d0865fb4454c403d3f2f670d0f132d8dcd
Tingyu Su <tingyusu@tingyus-mbp.wireless.newschool.edu>
2019-10-03 17:50:09 -0400

    testing2

--- testing/Thesis_Oct3/main.js
+++ testing/Thesis_Oct3/main.js
@@ -1,3 +1,15 @@
+function setup(){
+
+    createCanvas(windowWidth,580);
+
+    var button = select("#submit");
+    button.mousePressed(weatherAsk);
+
+    input = select("#city");
+    fill(0);
+    siz = 10;
+}
+
 function saveFunction(){
     save('myThesisOct3.jpg');
 }
